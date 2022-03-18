19d0887fa702bccb8e3a90d3352748e2e086edd9
Austin Evans <austinjevans@me.com>
2022-03-17 21:57:49 -0400

    Fix username alignment attempt 1

--- Sources/SwiftyChat/ChatMessageCellContainer.swift
+++ Sources/SwiftyChat/ChatMessageCellContainer.swift
@@ -22,84 +22,81 @@ internal struct ChatMessageCellContainer<Message: ChatMessage>: View {
     public let contactFooterSection: (ContactItem, Message) -> [ContactCellButton]
     public let onTextTappedCallback: () -> AttributedTextTappedCallback
     public let onCarouselItemAction: (CarouselItemButton, Message) -> Void
-
-    @ViewBuilder private func usernameHeader() -> some View {
-        Text(message.user.userName)
-            .fontWeight(usernameStyle.textStyle.fontWeight)
-            .font(usernameStyle.textStyle.font)
-            .foregroundColor(usernameStyle.textStyle.textColor)
-    }
     
     @ViewBuilder private func messageCell() -> some View {
-        if usernameStyle.showUsername {
-            usernameHeader()
-        }
+        VStack(alignment: message.isSender ? .trailing : .leading) {
+            if usernameStyle.showUsername {
+                Text(message.user.userName)
+                    .fontWeight(usernameStyle.textStyle.fontWeight)
+                    .font(usernameStyle.textStyle.font)
+                    .foregroundColor(usernameStyle.textStyle.textColor)
+            }
 
-        switch message.messageKind {
+            switch message.messageKind {
 
-        case .text(let text):
-            TextCell(
-                text: text,
-                message: message,
-                size: size,
-                callback: onTextTappedCallback
-            )
-            
-        case .location(let location):
-            LocationCell(
-                location: location,
-                message: message,
-                size: size
-            )
-            
-        case .imageText(let imageLoadingType, let text):
-            ImageTextCell(
-                message: message,
-                imageLoadingType: imageLoadingType,
-                text: text,
-                size: size
-            )
-            
-        case .image(let imageLoadingType):
-            ImageCell(
-                message: message,
-                imageLoadingType: imageLoadingType,
-                size: size
-            )
-            
-        case .contact(let contact):
-            ContactCell(
-                contact: contact,
-                message: message,
-                size: size,
-                footerSection: contactFooterSection
-            )
-            
-        case .quickReply(let quickReplies):
-            QuickReplyCell(
-                quickReplies: quickReplies,
-                quickReplySelected: onQuickReplyItemSelected
-            )
-            
-        case .carousel(let carouselItems):
-            CarouselCell(
-                carouselItems: carouselItems,
-                size: size,
-                message: message,
-                onCarouselItemAction: onCarouselItemAction
-            )
-            
-        case .video(let videoItem):
-            VideoPlaceholderCell(
-                media: videoItem,
-                message: message,
-                size: size
-            )
-            
-        case .loading:
-            LoadingCell(message: message, size: size)
+            case .text(let text):
+                TextCell(
+                    text: text,
+                    message: message,
+                    size: size,
+                    callback: onTextTappedCallback
+                )
+
+            case .location(let location):
+                LocationCell(
+                    location: location,
+                    message: message,
+                    size: size
+                )
+
+            case .imageText(let imageLoadingType, let text):
+                ImageTextCell(
+                    message: message,
+                    imageLoadingType: imageLoadingType,
+                    text: text,
+                    size: size
+                )
+
+            case .image(let imageLoadingType):
+                ImageCell(
+                    message: message,
+                    imageLoadingType: imageLoadingType,
+                    size: size
+                )
+
+            case .contact(let contact):
+                ContactCell(
+                    contact: contact,
+                    message: message,
+                    size: size,
+                    footerSection: contactFooterSection
+                )
+
+            case .quickReply(let quickReplies):
+                QuickReplyCell(
+                    quickReplies: quickReplies,
+                    quickReplySelected: onQuickReplyItemSelected
+                )
+
+            case .carousel(let carouselItems):
+                CarouselCell(
+                    carouselItems: carouselItems,
+                    size: size,
+                    message: message,
+                    onCarouselItemAction: onCarouselItemAction
+                )
+
+            case .video(let videoItem):
+                VideoPlaceholderCell(
+                    media: videoItem,
+                    message: message,
+                    size: size
+                )
+
+            case .loading:
+                LoadingCell(message: message, size: size)
+            }
         }
-        
     }
     
     public var body: some View {
